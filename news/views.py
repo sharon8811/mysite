63791124a51563e1802c42f5be86c8217f2197ef
sharon <sharon8811@gmail.com>
2016-04-24 10:20:01 +0300

    first version1.1

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponse
 from .models import Article, ArticleImages
+from django.db.models import Q
 
 
 # Create your views here.
@@ -12,14 +13,14 @@ def index(request):
         else:
             search_text = ''
         if search_text != '':
-            news = Article.objects.filter(name__icontains=search_text)
+            news = Article.objects.filter(Q(name__icontains=search_text) | Q(writer__icontains=search_text)).order_by('-date')
         else:
             news = ''
         return render_to_response('news/ajax_search.html', {'news': news})
     else:
         template_name = 'news/index.html'
         news_list = Article.objects.order_by('-date')
-        return render(request, template_name,  {'news_list': news_list})
+        return render(request, template_name,  {'news_list': news_list, 'author_page': False})
 
 
 def article(request, article_id):
@@ -35,4 +36,4 @@ def article(request, article_id):
 def author(request, author_name):
     template_name = 'news/index.html'
     news_list = Article.objects.filter(writer=author_name).order_by('-date')
-    return render(request, template_name, {'news_list': news_list})
+    return render(request, template_name, {'news_list': news_list, 'author_page': True, 'author_name': author_name})
