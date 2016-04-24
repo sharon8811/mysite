871580e72c33421e141eaf4cdead16e8007401dd
sharon <sharon8811@gmail.com>
2016-04-24 11:03:51 +0300

    first version1.1

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,6 @@ app_name = "news"
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^article/([0-9]+)/$', views.article, name='article'),
-    url(r'^author/(?P<author_name>[-.\w\s]+)/$', views.author, name='author'),
+    url(r'^author/(?P<author_name>[-."\'\w\s]+)/$', views.author, name='author'),
     url(r'^submit/$', views.submit, name='submit'),
 ]
